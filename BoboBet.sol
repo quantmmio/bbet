9849d9195671dcccca258751bf1516d3d44a4497
mato28 <matic.tempfer@gmail.com>
2024-04-10 16:56:12 +0200

    Change OZ version

--- BoboBet.sol
+++ BoboBet.sol
@@ -44,6 +44,18 @@ contract BoboBet is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, AccessControl
         _updatetaxdestination(newdestination);
     }
 
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        super._mint(account, amount);
+    }
+
     function _transfer(address from, address to, uint256 amount)
     internal
     virtual
